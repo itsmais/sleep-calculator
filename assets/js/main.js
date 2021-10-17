f379a12b04620a7bc4bdcc2e0ac0511325192a2b
itsmais <mih28@mail.aub.edu>
2021-10-17 16:55:05 +0200

    design enhancement

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,15 @@
 let sleepResult = document.getElementById("sleep-result");
 
-function sleepNow(){
+function sleepNow() {
     let date = new Date();
     let hours = date.getHours();
     let minutes = date.getMinutes();
-    
+
     // 5 cycles
     date.setMinutes(minutes + 450)
     let idealHours = 0;
     let idealMinutes = 0;
-    sleepResult.innerHTML="</br>It's "+hours+":"+minutes+" now."
-    + " You should <span class=\"time-result\">set your alarm at "+date.getHours()+":"+date.getMinutes()+"</span>"
-    +"</br>to complete five 90-minute sleep cycles.";
+    sleepResult.innerHTML = "</br>It's " + hours + ":" + minutes + " now." +
+        " You should <div class=\"time-result\">set your alarm at " + ('0' + date.getHours()).slice(-2) + ":" + ('0' + date.getMinutes()).slice(-2) + "</div>" +
+        "to complete five 90-minute sleep cycles.";
 }
